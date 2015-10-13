50d818950ccf7892afa18f880a5cbb5dd81715ba
ssaemo <jeontha@gmail.com>
2015-10-13 15:14:00 +0900

    window2_commit

---
+++ window2/window2.cpp
@@ -0,0 +1,17 @@
+// window2.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+	char str[100];
+
+	scanf("%100s", str);
+
+	printf("%s\n", str);
+
+	return 0;
+}
+
